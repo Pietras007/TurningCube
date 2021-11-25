c9e9dd70a7c3440b3b0d68bce44886fd7b7cd3ea
Piotr <piotr.samborski007@gmail.com>
2021-11-25 19:33:38 +0100

    fix start position

--- Geometric2/Global/GlobalPhysicsData.cs
+++ Geometric2/Global/GlobalPhysicsData.cs
@@ -24,7 +24,7 @@ namespace Geometric2.Global
         public InitialConditionsData InitialConditionsData = new InitialConditionsData();
 
         //Visualization Data
-        public double alfaAngleInRadian = 0.5;
+        public double alfaAngleInRadian = (double)(Math.PI / 180) * 15;
         public double diagonalRoundInRadian = 0.0;
         public double yRoundInRadian = 0.0;
     }
